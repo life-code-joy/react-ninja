3f06ac202a6aaa5929d53601941e70284ec41eae
darren <darren__c@hotmail.com>
2021-06-15 14:58:47 +0900

    click funtion and the e and anonymous function for argumants

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log('clicked', e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log('I got clicked my dear friend ... ', name, e.target);
+  };
+
   return (
     <div className='home'>
       <h2 className='home-style-heading'>Homepage</h2>
+      <button onClick={handleClick}>click me</button>
+      <button
+        onClick={(e) => {
+          handleClickAgain('Mario', e);
+        }}
+      >
+        click me
+      </button>
     </div>
   );
 };
