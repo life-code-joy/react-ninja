df59395f32c3b27bed083a3780e54804a45ab93c
darren <darren__c@hotmail.com>
2021-06-15 10:40:17 +0900

    deleted App.css,will use index.css inline css example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import './App.css';
-import './Navbar';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const info = 'My address is 54 longwood.';
-  const list = [12, 34, 22, 55, 23];
-  // const link = 'http://www.google.com';
-  // no object or booleans can be outputed
   return (
-    <div className='app'>
-      <h1>App Component</h1>
-      <p>{info}</p>
-      <p>{list}</p>
-      <p>{67777}</p>
-      <p>{'this is a list'}</p>
-      {/* expressions can be outputted */}
-      <p>{Math.random() * 10}</p>
-      <p>{['one ', 'two ', 'three']}</p>
-      <a href='{link}'>Google</a>
-      {/* <Navbar /> */}
+    <div className=''>
+      <Navbar />
+      <div className='content'>
+        <Home />
+      </div>
     </div>
   );
 }
