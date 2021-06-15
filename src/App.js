0586414ac3d48f75b8e316bab4a0a13dc9e8a83e
darren <darren__c@hotmail.com>
2021-06-15 09:47:06 +0900

    dynamic output in App.js experiment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import './Navbar';
 
 function App() {
+  const info = 'My address is 54 longwood.';
+  const list = [12, 34, 22, 55, 23];
+  // const link = 'http://www.google.com';
+  // no object or booleans can be outputed
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>App Component</h1>
+      <p>{info}</p>
+      <p>{list}</p>
+      <p>{67777}</p>
+      <p>{'this is a list'}</p>
+      {/* expressions can be outputted */}
+      <p>{Math.random() * 10}</p>
+      <p>{['one ', 'two ', 'three']}</p>
+      <a href='{link}'>Google</a>
+      {/* <Navbar /> */}
     </div>
   );
 }
