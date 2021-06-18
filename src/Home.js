8f2252119c37fba53e8c44c774b978b304c6efcf
darren <darren__c@hotmail.com>
2021-06-18 15:38:37 +0900

    fetch from db.json using a json server on port 8000

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      blogName: "Horray it's lunch time!!",
-      author: 'lilian Beth',
-      body: '.....',
-      id: 1,
-    },
-    {
-      blogName: 'Nice job!!',
-      author: 'Hath Livingston',
-      body: '.....',
-      id: 2,
-    },
-    {
-      blogName: 'Time for chigong',
-      author: 'darren',
-      body: '.....',
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    // console.log('run use effect');
+    fetch('http://localhost:8000/blogs')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
   return (
     <div className='home'>
-      <BlogList blogs={blogs} title='All blogs' handleDelete={handleDelete} />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === 'darren')}
-        title='darren blogs'
-      />
+      {blogs && <BlogList blogs={blogs} title='All blogs' />}
     </div>
   );
 };
