7b6b84b4d275f7bab523af6a2008ac4ef8a2ae45
darren <darren__c@hotmail.com>
2021-06-18 13:20:25 +0900

    delete posts handleDelete

--- src/Home.js
+++ src/Home.js
@@ -22,9 +22,13 @@ const Home = () => {
       id: 3,
     },
   ]);
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
   return (
     <div className='home'>
-      <BlogList blogs={blogs} title='All blogs' />
+      <BlogList blogs={blogs} title='All blogs' handleDelete={handleDelete} />
       <BlogList
         blogs={blogs.filter((blog) => blog.author === 'darren')}
         title='darren blogs'
