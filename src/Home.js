d23852b4e4ed029530fb38920ba76f29f6174938
darren <darren__c@hotmail.com>
2021-06-19 12:48:07 +0900

    catch error and displayed in browser

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,8 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
@@ -10,17 +12,28 @@ const Home = () => {
 
   useEffect(() => {
     // console.log('run use effect');
-    fetch('http://localhost:8000/blogs')
+    fetch('http://localhost:8000/blogss')
       .then((res) => {
+        if (!res.ok) {
+          throw Error('oh oh spagetti O');
+          // console.log('no dice');
+        }
         return res.json();
       })
       .then((data) => {
         console.log(data);
         setBlogs(data);
+        setIsPending(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setError(err.message);
       });
   }, []);
   return (
     <div className='home'>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading plese wait ...</div>}
       {blogs && <BlogList blogs={blogs} title='All blogs' />}
     </div>
   );
